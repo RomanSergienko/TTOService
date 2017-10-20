94f73ca05304191fdc851c0454b6844fc205b581
Roman Sergienko <r-sergienko@ya.ru>
2017-10-20 16:28:49 +0400

    Add method findAllRefType

--- src/main/java/com/tto/project/service/TtoService.java
+++ src/main/java/com/tto/project/service/TtoService.java
@@ -1,14 +1,35 @@
 package com.tto.project.service;
 
+import com.tto.project.entity.RefRepresentationType;
 import com.tto.project.entity.Ttd;
 import com.tto.project.entity.Tto;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by RomanDev on 18.10.2017.
  */
 public interface TtoService {
-    List<Ttd> findTopology (String ttoid, Integer reprtypecode, List<String> refcodes);
-    List<Tto> findStuctures(String ttdUUID);
+    /**
+     * Возращает список топологий
+     * @param ttdname
+     * @param reprtypecode
+     * @param refcodes
+     * @return
+     */
+    List<Ttd> findTopology (String ttdname, Integer reprtypecode, List<String> refcodes, Boolean isUsedLike);
+
+    /**
+     * Возращает список кодов материалов с данной топологией
+     * @param ttdUUID
+     * @return
+     */
+    List<Tto> findStuctures(UUID ttdUUID);
+
+    /**
+     * Возвращает все справочники
+     * @return
+     */
+    List<RefRepresentationType> findAllRefType();
 }

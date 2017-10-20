32e83bb0f1b3a1e75c543fe0b5c36cb91906926f
Roman Sergienko <r-sergienko@ya.ru>
2017-10-20 10:30:58 +0400

    Add sessionfactory bean

--- src/main/java/com/tto/project/TestApplication.java
+++ src/main/java/com/tto/project/TestApplication.java
@@ -2,11 +2,18 @@ package com.tto.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 
 @SpringBootApplication
 public class TestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
+	}
+
+	@Bean
+	public HibernateJpaSessionFactoryBean sessionFactory() {
+		return new HibernateJpaSessionFactoryBean();
 	}
 }

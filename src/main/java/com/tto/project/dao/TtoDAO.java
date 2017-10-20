50f997fccd558b1ec45f53772f5997be22918af6
Roman Sergienko <r-sergienko@ya.ru>
2017-10-20 12:48:16 +0400

    Add method findAllRefType

--- src/main/java/com/tto/project/dao/TtoDAO.java
+++ src/main/java/com/tto/project/dao/TtoDAO.java
@@ -1,15 +1,18 @@
 package com.tto.project.dao;
 
+import com.tto.project.entity.RefRepresentationType;
 import com.tto.project.entity.Ttd;
 import com.tto.project.entity.Tto;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by RomanDev on 18.10.2017.
  */
 public interface TtoDAO {
-    List<Ttd> findTopology (String ttoid, Integer reprtypecode, List<String> refcodes);
-    List<Tto> findStuctures(String ttdUUID);
+    List<Ttd> findTopology (String ttdname, Integer reprtypecode, List<String> refcodes,Boolean isUsedLike);
+    List<Tto> findStuctures(UUID ttdUUID);
+    List<RefRepresentationType> findAllRefType();
 
 }
--- src/main/java/com/tto/project/dao/TtoDAOHibernateImpl.java
+++ src/main/java/com/tto/project/dao/TtoDAOHibernateImpl.java
@@ -1,17 +1,22 @@
 package com.tto.project.dao;
 
+import com.tto.project.entity.RefRepresentationType;
 import com.tto.project.entity.Ttd;
 import com.tto.project.entity.Tto;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by RomanDev on 19.10.2017.
  */
 @Repository
+@Transactional(readOnly = true)
 public class TtoDAOHibernateImpl implements TtoDAO{
     private SessionFactory sessionFactory;
     @Autowired
@@ -20,12 +25,19 @@ public class TtoDAOHibernateImpl implements TtoDAO{
     }
 
     @Override
-    public List<Ttd> findTopology(String ttoid, Integer reprtypecode, List<String> refcodes) {
+    public List<Ttd> findTopology(String ttdname, Integer reprtypecode, List<String> refcodes, Boolean isUsedLike) {
         return null;
     }
 
     @Override
-    public List<Tto> findStuctures(String ttdUUID) {
+    public List<Tto> findStuctures(UUID ttdUUID) {
         return null;
+    }
+
+    @Override
+    public List<RefRepresentationType> findAllRefType() {
+        Session session = sessionFactory.getCurrentSession();
+        List<RefRepresentationType> typeList = session.createCriteria(RefRepresentationType.class).list();
+        return typeList;
     }
 }

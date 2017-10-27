bf3167784f269ce26cbaa8ff9a15c6411dc71048
Roman Sergienko <r-sergienko@ya.ru>
2017-10-27 10:59:29 +0400

    Change pathvariable on requestparam

--- src/main/java/com/tto/project/controller/TtoController.java
+++ src/main/java/com/tto/project/controller/TtoController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -25,16 +22,16 @@ public class TtoController {
     TtoService ttoService;
 
     @RequestMapping(value = "/ttd", method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Ttd>> listTtd(@PathVariable("ttoid") String ttdname,
-                                             @PathVariable("reprtypecode")Integer reprtypecode,
-                                             @PathVariable("refcodes")List<String> refcodes,
-                                             @PathVariable("like")Boolean isUsedLike){
+    public ResponseEntity<List<Ttd>> listTtd(@RequestParam("ttdname") String ttdname,
+                                             @RequestParam("reprtypecode")Integer reprtypecode,
+                                             @RequestParam("refcodes")List<String> refcodes,
+                                             @RequestParam("like")Boolean isUsedLike){
         List<Ttd> ttdList = ttoService.findTopology(ttdname, reprtypecode, refcodes,isUsedLike);
         return new ResponseEntity<List<Ttd>>(ttdList, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/tto", method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Tto>> listTto(@PathVariable("ttdUUID")UUID ttdUUID){
+    public ResponseEntity<List<Tto>> listTto(@RequestParam("ttdUUID")UUID ttdUUID){
         List<Tto> ttoList = ttoService.findStuctures(ttdUUID);
         return new ResponseEntity<List<Tto>>(ttoList,HttpStatus.OK);
     }

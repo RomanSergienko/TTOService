2a27208f0ee4cda959f0a490e900dd80d6e2262e
Roman Sergienko <r-sergienko@ya.ru>
2017-10-27 10:58:27 +0400

    Add transactional

--- src/main/java/com/tto/project/service/TtoServiceImpl.java
+++ src/main/java/com/tto/project/service/TtoServiceImpl.java
@@ -6,6 +6,7 @@ import com.tto.project.entity.Ttd;
 import com.tto.project.entity.Tto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -21,16 +22,17 @@ public class TtoServiceImpl implements TtoService {
         this.ttoDAO = ttoDAO;
     }
 
+    @Transactional
     @Override
     public List<Ttd> findTopology(String ttoid, Integer reprtypecode, List<String> refcodes,Boolean isUsedLike) {
         return ttoDAO.findTopology(ttoid,reprtypecode,refcodes,isUsedLike);
     }
-
+    @Transactional
     @Override
     public List<Tto> findStuctures(UUID ttdUUID) {
         return ttoDAO.findStuctures(ttdUUID);
     }
-
+    @Transactional
     @Override
     public List<RefRepresentationType> findAllRefType() {
         return ttoDAO.findAllRefType();

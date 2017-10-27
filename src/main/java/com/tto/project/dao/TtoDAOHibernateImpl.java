c2051abcf1d579ca42c04dfab24f96fe84e56c2a
Roman Sergienko <r-sergienko@ya.ru>
2017-10-27 13:59:47 +0400

    Add Criteria implementing of methods

--- src/main/java/com/tto/project/dao/TtoDAOHibernateImpl.java
+++ src/main/java/com/tto/project/dao/TtoDAOHibernateImpl.java
@@ -3,8 +3,10 @@ package com.tto.project.dao;
 import com.tto.project.entity.RefRepresentationType;
 import com.tto.project.entity.Ttd;
 import com.tto.project.entity.Tto;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,18 +28,54 @@ public class TtoDAOHibernateImpl implements TtoDAO{
 
     @Override
     public List<Ttd> findTopology(String ttdname, Integer reprtypecode, List<String> refcodes, Boolean isUsedLike) {
-        return null;
+
+        Session session = sessionFactory.getCurrentSession();
+        Criteria cr = session.createCriteria(Ttd.class,"td");
+
+
+        if (!ttdname.isEmpty() || !ttdname.equals("")){
+            if (isUsedLike){
+                cr.add(Restrictions.like("name","%"+ttdname+"%"));
+            }else {
+                cr.add(Restrictions.eq("name",ttdname));
+            }
+
+        }
+        if (reprtypecode!=null || !refcodes.isEmpty()){
+
+        }
+        //by refcode and ttdname
+        //select * from ttd join tto on ttd.id=tto.ttdid where ttd.name like ttdname and tto.refcode=refcodes
+        // by tto.reprtypecode
+        //select * from ttd  join tto on ttd.id = tto.ttid left join ref_representation_type
+        // on tto.reprtyprcode=ref_representation_type.id
+
+        List result = cr.list();
+        return result;
     }
 
     @Override
     public List<Tto> findStuctures(UUID ttdUUID) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        Criteria criteria = session.createCriteria(Tto.class);
+        criteria.add(Restrictions.eq("ttdid",ttdUUID));
+        List<Tto> result = criteria.list();
+        return result;
     }
 
     @Override
     public List<RefRepresentationType> findAllRefType() {
         Session session = sessionFactory.getCurrentSession();
         List<RefRepresentationType> typeList = session.createCriteria(RefRepresentationType.class).list();
+//        Ttd ttd = new Ttd();
+//        ttd.setId(UUID.randomUUID());
+//        ttd.setName("diamond");
+//        ttd.setTtdcode(10);
+//        ttd.setTs("Строка");
+//        ttd.setNumnodes(111);
+//        ttd.setCreated(new Timestamp(Calendar.getInstance().getTimeInMillis()));
+//        ttd.setUpdated(new Timestamp(Calendar.getInstance().getTimeInMillis()));
+//        session.save(ttd);
         return typeList;
     }
 }
